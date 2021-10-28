37f8797e833757da7c6f534cb32a47ef3a0ed515
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-10-28 20:18:32 -0300

    file with simulation of data to render cards

---
+++ personal-page/src/data/arrHighlights.js
@@ -0,0 +1,34 @@
+const arrDataCardsHighlights = [
+  {
+    thumbnail: 'link da imagem a ser renderizada no componente',
+    title: 'string qualquer',
+    description: 'A short description about this project',
+    tags: '#React, #JavascriptPuro, #Redux, #ContextAPI',
+  },
+  {
+    thumbnail: 'link da imagem a ser renderizada no componente',
+    title: 'string qualquer',
+    description: 'A short description about this project',
+    tags: '#React, #JavascriptPuro, #Redux, #ContextAPI',
+  },
+  {
+    thumbnail: 'link da imagem a ser renderizada no componente',
+    title: 'string qualquer',
+    description: 'A short description about this project',
+    tags: '#React, #JavascriptPuro, #Redux, #ContextAPI',
+  },
+  {
+    thumbnail: 'link da imagem a ser renderizada no componente',
+    title: 'string qualquer',
+    description: 'A short description about this project',
+    tags: '#React, #JavascriptPuro, #Redux, #ContextAPI',
+  },
+  {
+    thumbnail: 'link da imagem a ser renderizada no componente',
+    title: 'string qualquer',
+    description: 'A short description about this project',
+    tags: '#React, #JavascriptPuro, #Redux, #ContextAPI',
+  },
+];
+
+export default arrDataCardsHighlights;
