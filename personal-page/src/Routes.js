ebf13208388e5434d79d5554221a67d2a2e1a444
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-10-28 20:11:02 -0300

    fixing warnings in Routes file

--- personal-page/src/Routes.js
+++ personal-page/src/Routes.js
@@ -2,23 +2,23 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import {
   Home,
-  LetsTalk,
-  AboutMe,
-  Portfolio,
-  FrontEnd,
-  BackEnd,
+  // LetsTalk,
+  // AboutMe,
+  // Portfolio,
+  // FrontEnd,
+  // BackEnd,
   Repositories,
 } from './pages';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
-    <Route exact path="/lesttalk" component={LetsTalk} />
-    <Route exact path="/aboutme" component={AboutMe} />
+    {/* <Route exact path="/lesttalk" component={LetsTalk} /> */}
+    {/* <Route exact path="/aboutme" component={AboutMe} /> */}
     <Route exact path="/repositories" component={Repositories} />
-    <Route exact path="/portfolio" component={Portfolio} />
-    <Route exact path="/portfolio/front-end" component={FrontEnd} />
-    <Route exact path="/portfolio/back-end" component={BackEnd} />
+    {/* <Route exact path="/portfolio" component={Portfolio} /> */}
+    {/* <Route exact path="/portfolio/front-end" component={FrontEnd} /> */}
+    {/* <Route exact path="/portfolio/back-end" component={BackEnd} /> */}
   </Switch>
 );
 
